cffe827b6287a6fc0a5fb2bee9ba4089365d0565
corinnelhh <corinne.hutchinson@gmail.com>
2014-06-23 11:07:36 -0700

    first commit: skeleton outline and default training file

---
+++ chatbot_brain.py
@@ -0,0 +1,20 @@
+import nltk, random
+
+class Chatbot(object):
+
+    def __init__(self, training_file="tell_tale_heart.txt"):
+        self.training_file = training_file
+        self.lexicon = {}
+
+    def read_training_input(training_file):
+
+
+    def tokenize_input(input):
+
+
+    def tag_input(input):
+
+
+    def fill_lexicon(training_file):
+
+

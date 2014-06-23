0a06f97a5ce788f275ca8aaeb09c158be9359955
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-23 11:41:00 -0700

    added routing function for text submission

--- views.py
+++ views.py
@@ -6,11 +6,25 @@ from flask import request
 from flask import url_for
 from flask import redirect
 from flask import session
+import chatbot_brain
 
+app = Flask(__name__)
 
 @app.route('/')
 def show_chatbot():
-    return render_template('base.html')
+    """Displays base.html when user goes to main page."""
+    return render_template('base.html', reply=reply)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    """Accepts user submission, creates reply, redirects to homepage."""
+    try:
+        submission = request.form['submission']
+        reply = cbot.<"analyze the input">(submission)
+    return redirect(url_for('show_chatbot'))
+
 
 if __name__ == '__main__':
+    cbot = chatbot_brain.Chatbot()
     app.run(debug=True)

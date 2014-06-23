b2acb8161d56892fbd5ce10bc4c41016956b11d0
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-23 14:33:26 -0700

    changed 'if main = name' block to run a wsgi app

--- views.py
+++ views.py
@@ -25,6 +25,30 @@ def submit():
     return show_chatbot(reply)
 
 
+def application(environ, start_response):
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body]
+
+
 if __name__ == '__main__':
     #cbot = chatbot_brain.Chatbot()
-    app.run(debug=True)
+    #app.run(debug=True)
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8000, app)
+    srv.serve_forever()

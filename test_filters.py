b4900c6532a0fd8d45cc03497fc2fbb3f3a03f19
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-24 15:00:50 -0700

    added a test for the new output filter, tests may need refactoring

--- test_filters.py
+++ test_filters.py
@@ -7,8 +7,6 @@ sentences = ["good breeding and of the match Lady Catherine is not know and let 
             "may I entertain you with a list of seasonal chocolates",
             "perhaps your kindness shall be rewarded"]
 
-
-
 def test_length():
     reduced_sentences = output_filters.filter_length(sentences)
     assert len(reduced_sentences) == 4
@@ -17,3 +15,17 @@ def test_length_null():
     reduced_sentences = output_filters.filter_length(sentences)
     same_sentences = output_filters.filter_length(reduced_sentences)
     assert len(same_sentences) == len(reduced_sentences)
+
+def test_filter_pos():
+    sentences = ["good breeding and of the match Lady Catherine is not know and let us the express To morrow fortnight and by any thing in a month You see it and sometimes made the very gravely glancing.",
+            "here are to the company I congratulate you have seen Collins and he had recommended him from Jane I dare say",
+            "hello my friend I hope you are well",
+            "good evening gracious lady you are most considerate",
+            "may I entertain you with a list of seasonal chocolates",
+            "perhaps your kindness shall be rewarded"]
+    reduced_sentences = output_filters.filter_pos(sentences)
+    assert len(reduced_sentences) == 6
+
+
+
+

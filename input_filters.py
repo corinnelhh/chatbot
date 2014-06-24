b0d14aeb48f03f3ef908fe51158751f8cd71e04b
corinnelhh <corinne.hutchinson@gmail.com>
2014-06-24 13:05:19 -0700

    updates to small talk filter

--- input_filters.py
+++ input_filters.py
@@ -1,10 +1,15 @@
 import nltk
 from nltk.tokenize import wordpunct_tokenize
+import random
 
 
 def i_filter_small_talk_typer(input_):
-    input_ = wordpunct_tokenize(input_)
-
+    if wordpunct_tokenize(input_)[:2] == ["It", "means"]
+        return i_filter_small_talk('new_word')
+    elif "weather" in input_:
+        return 'weather'
+    else:
+        return _input
 
 
 def i_filter_small_talk(dict_key, keyword=None):
@@ -17,10 +22,19 @@ def i_filter_small_talk(dict_key, keyword=None):
     small_talk_dict = {'unknown_word' : ["What a funny thing to say!",
         "That's a new one!", "Huh!"],'weather' :
         ["Talking about the weather is such a bore.",
-        "I'm not the weatherman!"], 'feelings' : ["Why do you care how I feel?",
-        "More importantly, how do you feel?", "Really, are *you* {}".format()],
-        'sports' : ["You're a sports fan!", "I've never been much of an athlete..."],
+        "I'm not the weatherman!"], 'feelings' :
+        ["Why do you care how I feel?",
+        "More importantly, how do you feel?",
+        "Really, are *you* {}".format()],
+        'sports' : ["You're a sports fan!",
+        "I've never been much of an athlete..."],
         'new_word': ['Wow, thanks for explaining that.',
     }
 
-    if 
+    while True:
+        response = random.choice(small_talk_dict[dict_key])
+        if 
+
+
+if __name__ == "__main__":
+

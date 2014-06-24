4039f713906bc39f25acf05940f9f7d1f35e87fa
corinnelhh <corinne.hutchinson@gmail.com>
2014-06-24 14:44:05 -0700

    incorporated first input filter

--- input_filters.py
+++ input_filters.py
@@ -2,12 +2,9 @@ import nltk
 from nltk import pos_tag
 from nltk.tokenize import wordpunct_tokenize
 import random
-from trainbot import Trainbot
 
 
-def i_filter_small_talk(input_):
-    bot = Trainbot()
-    bot.fill_lexicon()
+def i_filter_small_talk(input_, lexicon=None):
     sports_words = ["basketball", "soccer", "football", "baseball",
             "hockey", "tennis"]
     weather_words = ["weather", "raining", "rain", "snowing", "snows",
@@ -22,11 +19,10 @@ def i_filter_small_talk(input_):
             return small_talk_dict('weather')
         elif word in feeling_words:
             return small_talk_dict('feelings')
-        elif word not in bot.bi_lexicon:
+        elif word not in lexicon:
             return small_talk_dict('unknown_word')
     else:
-        no_small = True
-        return input_, no_small
+        return input_
 
 
 def small_talk_dict(dict_key, keyword=None):
@@ -53,30 +49,24 @@ def small_talk_dict(dict_key, keyword=None):
 def token(string_):
     return wordpunct_tokenize(string_)
 
-def filter_content(words):
+
+def filter_content(words, lexicon=None):
     """Takes in a list of words and returns a list
 
     of the nouns, verbs, and adjectives the orig list contained."""
     tagged = pos_tag(words)
     possible_seeds = []
-    content_pos = ['VV','NN','JJ']
-    for word,pos in tagged:
+    content_pos = ['VV', 'NN', 'JJ']
+    for word, pos in tagged:
         if pos[:2] in content_pos:
             possible_seeds.append(word)
     return possible_seeds
 
 
-def filter_length_words(words):
-    """Takes in a list of words and returns all words longer than two letters."""
+def filter_length_words(words, lexicon=None):
+    "Takes in a list of words and returns all words longer than two letters."
     possible_seeds = []
     for word in words:
-        if len(word) >2:
+        if len(word) > 2:
             possible_seeds.append(word)
     return possible_seeds
-
-
-if __name__ == "__main__":
-    tokenized = wordpunct_tokenize("How old are you?")
-    print i_filter_small_talk(tokenized)
-    print filter_content(token("I went running yesterday"))
-

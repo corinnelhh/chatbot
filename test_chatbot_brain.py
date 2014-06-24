c675a0da19534d0bec9bc88b5699418250373454
corinnelhh <corinne.hutchinson@gmail.com>
2014-06-24 10:31:28 -0700

    added test for output

--- test_chatbot_brain.py
+++ test_chatbot_brain.py
@@ -3,18 +3,18 @@ import pytest
 import chatbot_brain
 
 def test_initialize_bot():
-    bot = Chatbot()
+    bot = chatbot_brain.Chatbot()
     assert len(bot.tri_lexicon) == 0
     assert len(bot.bi_lexicon) == 0
 
 def test_fill_lexicon():
-    bot = Chatbot()
+    bot = chatbot_brain.Chatbot()
     bot.fill_lexicon()
     assert len(bot.tri_lexicon) > 0
     assert len(bot.bi_lexicon) > 0
 
 
 def test_create_chains():
-    bot = Chatbot()
-    output = bot.create_chains()
+    bot = chatbot_brain.Chatbot()
+    output = bot.generate_response("How are you doing?")
     assert "," not in output[0]

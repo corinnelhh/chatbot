ea8f06df1033961c44ee7ac8d6c0f50fea1174d4
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-24 09:56:51 -0700

    defined length output filter with doc string

---
+++ output_filters.py
@@ -0,0 +1,5 @@
+def filter_length(sentences, wordcount=13):
+    """Takes in a list of sentences and returns a reduced list,
+
+    that contains only sentences with less than <wordcount> words."""
+

979ab9ee39b6a40dbe2d036d25bc20e363f82339
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-24 10:03:29 -0700

    started a test file with a list of example sentences

---
+++ test_filters.py
@@ -0,0 +1,11 @@
+import output_filters
+
+sentences = ["good breeding and of the match Lady Catherine is not know and let us the express To morrow fortnight and by any thing in a month You see it and sometimes made the very gravely glancing.",
+            "here are to the company I congratulate you have seen Collins and he had recommended him from Jane I dare say",
+            "hello my friend I hope you are well",
+            "good evening gracious lady you are most considerate",
+            "may I entertain you with a list of seasonal chocolates",
+            "perhaps your kindness shall be rewarded"]
+
+
+

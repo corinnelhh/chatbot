1de25eedf8a354de38d4c0c062f239d173220e44
jefrailey <jcaetof@gmail.com>
2014-06-24 18:24:17 -0700

    Refactor Chatbot._create_chains() to be slimmer

--- chatbot_brain.py
+++ chatbot_brain.py
@@ -14,7 +14,7 @@ class Chatbot(Trainbot):
         super(Chatbot, self).__init__(training_file="tell_tale_heart.txt")
         self.training_file = training_file
 
-    def i_filter_random(self, words):
+    def i_filter_random(self, words, lexicon=None):
         u"""Return randomly selected, non-punctuation word from words."""
         count = 0
         while count < len(words):
@@ -28,32 +28,27 @@ class Chatbot(Trainbot):
         u"""Return randomly selected sentence from sentecnces"""
         return random.choice(sentences)
 
-    def _create_chains(self, pair, size=1):
+    def _create_chains(self, pair, size=10):
         u"""Return list of markov generated strings spawned from the seed."""
         candidates = []
-        # import pdb
-        # pdb.set_trace()
-        # while len(candidates) < size:
         word_1 = pair[0]
         word_2 = pair[1]
-        candidate = [word_1, word_2]
-        pair = "{} {}".format(word_1, word_2)
-        done = False
-        while not done:
-            try:
-                next_word = random.choice(self.tri_lexicon[pair])
-                candidate.append(next_word)
-                print candidate
-                word_1, word_2 = word_2, next_word
-                pair = "{} {}".format(word_1, word_2)
-            except KeyError:
-                candidate.append(next_word)
-                candidates.append(" ".join(candidate))
-                done = True
-            if next_word in self.stop_puncts:
-                candidate.append(next_word)
-                candidates.append(" ".join(candidate))
-                done = True
+        while len(candidates) < size:
+            candidate = [word_1, word_2]
+            pair = "{} {}".format(word_1, word_2)
+            done = False
+            while not done:
+                try:
+                    next_word = random.choice(self.tri_lexicon[pair])
+                    candidate.append(next_word)
+                    word_1, word_2 = word_2, next_word
+                    pair = "{} {}".format(word_1, word_2)
+                except KeyError:
+                    candidates.append(" ".join(candidate))
+                    done = True
+                if next_word in self.stop_puncts:
+                    candidates.append(" ".join(candidate))
+                    done = True
         return candidates
 
     def _pair_seed(self, seed):
@@ -67,31 +62,38 @@ class Chatbot(Trainbot):
                 continue
         return pair
 
-    def compose_response(self, input_sent, input_filter, output_filter, lexicon):
+    def compose_response(
+            self,
+            input_sent,
+            input_filter=None,
+            output_filter=None,
+            lexicon=None
+            ):
         u"""Return a response sentence based on the input."""
         # Tokenize input
         seeds = wordpunct_tokenize(input_sent)
         # Select seed based on input filter
-        seeds = input_filter(seeds, lexicon)
-            #If a default sentence was picked, return it.
-        if isinstance(seeds, basestring):
-            return seeds
+        if input_filter:
+            seeds = input_filter(seeds, lexicon)
+            # If a default sentence was picked, return it.
+            if isinstance(seeds, basestring):
+                return seeds
         # Randomly pick a seed from the returned possibilities.
-        print "seeds are: "
-        print seeds
         seed = self.i_filter_random(seeds)
         # Create chains
         pair = self._pair_seed(seed)
         chains = self._create_chains(pair)
         # Return output of filter
-        # return output_filter(chains)
-        return chains
+        filtered = output_filter(chains)
+        return filtered
 
 if __name__ == '__main__':
     bot = Chatbot()
     bot.fill_lexicon()
     print "Filled the lexicon!"
-    print bot.compose_response("My beautiful carriage is red and blue and it hums while I drive it!",
-        input_filters.filter_content_priority, bot.o_filter_random, bot.bi_lexicon)
-
-
+    print bot.compose_response(
+        "My beautiful carriage is red and blue and it hums while I drive it!",
+        input_filters.filter_content_priority,
+        bot.o_filter_random,
+        bot.bi_lexicon
+        )

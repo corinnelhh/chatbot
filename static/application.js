eed7f3a4e6845d30a2c4012ebe4ee830993330f2
jefrailey <jcaetof@gmail.com>
2014-06-25 11:20:31 -0700

    Update JS, styelsheet, and base.html to have a hidden output filter dropdown that shows when the value of the first dropdown is not the default.

--- static/application.js
+++ static/application.js
@@ -6,7 +6,14 @@ $(document).ready(function(){
     $('.show_hide').click(function(){
     $(".slidingDiv").slideToggle();
     });
-
+    $( "#output_filter" ).change(function() {
+    if ($("#output_filter").val() == "default") {
+        $("#output_filter2").hide();
+    }
+    else {
+        $("#output_filter2").show();
+    }
+    });
 });
 
 // Inspired by http://papermashup.com/simple-jquery-showhide-div/

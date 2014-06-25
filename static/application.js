d3f6af4906119fd0744bdd5d062068c2a74dab51
jefrailey <jcaetof@gmail.com>
2014-06-25 12:15:43 -0700

    Add third output filter dropdown that starts hidden and shows itself if a second filter is selected

--- static/application.js
+++ static/application.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
 
-        $(".slidingDiv").hide();
-        $(".show_hide").show();
+    $(".slidingDiv").hide();
+    $(".show_hide").show();
 
     $('.show_hide').click(function(){
     $(".slidingDiv").slideToggle();
     });
+
     $( "#output_filter" ).change(function() {
     if ($("#output_filter").val() == "default") {
         $("#output_filter2").hide();
     }
     else {
         $("#output_filter2").show();
+    }
+    });
+
+    $( "#output_filter2" ).change(function() {
+    if ($("#output_filter2").val() == "default") {
+        $("#output_filter3").hide();
+    }
+    else {
+        $("#output_filter3").show();
     }
     });
 });

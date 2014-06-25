d4e7bab420bea80c73fa17079b36c4c1abeb02b4
corinnelhh <corinne.hutchinson@gmail.com>
2014-06-25 11:45:46 -0700

    added POS ordered filter

--- output_filters.py
+++ output_filters.py
@@ -29,5 +29,23 @@ def filter_pos(sentences):
                 break
     return output_sentences
 
+def filter_NN_VV(sentences):
+    """Takes in a list of sentences and returns a reduced list of
+    sentences that have at least one noun followed somewhere by at least one verb
+    """
+    output_sentences = []
+    for sentence in sentences:
+        words = wordpunct_tokenize(sentence)
+        tagged = pos_tag(words)
+        for word, tag in tagged:
+            has_noun = False
+            if tag[:2] == "NN":
+                has_noun = True
+            if has_noun and tag[:2] == "VB":
+                output_sentences.append(sentence)
+                break
+    return output_sentences
+
+
 
 

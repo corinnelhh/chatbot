717364b1cb3ca7ac58f320385583ceaca3f66edb
jefrailey <jcaetof@gmail.com>
2014-06-25 20:31:56 -0700

    Add test sentences to test_chattbot_brain.py

--- test_chatbot_brain.py
+++ test_chatbot_brain.py
@@ -2,6 +2,23 @@ import chatbot_brain
 
 stock = u"What a funny thing to say!"
 
+sentences_ = [
+    "red coat myself very unwell this morning that has \
+    passed off much better of me if I speak plainer ?",
+    "red coat myself very unwell this morning that if it is .",
+    "red coat myself very unwell this morning and pack her trunk afresh.",
+    "red coat myself very unwell this morning that has passed off pleasantly \
+    to the following morning every hope of an illiterate and miserly father \
+    and mother .""",
+    "red coat myself very well madam said.",
+    "red coat myself very well together.",
+    "red coat myself very unwell this morning.",
+    "red coat myself very well for the first two months.",
+    "red coat myself very well that.",
+    "red coat myself very unwell this morning that if he comes\
+    into the regulars and among his former indolence."
+    ]
+
 
 def test_initialize_bot():
     bot = chatbot_brain.Chatbot()
@@ -32,7 +49,7 @@ def test_i_filter_random_empty_words():
 
 
 def test_i_filter_random_words_not_in_lexicon():
-    u"""Assert that if all words are not in lexicon the default is returned."""
+    u"""Assert if all words are not in lexicon the default is returned."""
     bot = chatbot_brain.Chatbot()
     words = ["moose", "bear", "eagle"]
     bot.bi_lexicon = {"car": "mercedes", "boat": "sail", "train": "track"}
@@ -40,7 +57,7 @@ def test_i_filter_random_words_not_in_lexicon():
 
 
 def test_i_filter_random_words_in_lexicon():
-    u"""Assert that if all words are in lexicon, a word is returned."""
+    u"""Assert if all words are in lexicon, a word is returned."""
     bot = chatbot_brain.Chatbot()
     words = ["car", "boat", "train"]
     bot.bi_lexicon = {"car": "mercedes", "boat": "sail", "train": "track"}
@@ -48,15 +65,22 @@ def test_i_filter_random_words_in_lexicon():
 
 
 def test_i_filter_random_one_word_in_lexicon():
-    u"""Assert that if one word is in lexicon that word is returned"""
+    u"""Assert if one word is in lexicon that word is returned"""
     bot = chatbot_brain.Chatbot()
     words = ["car", "bear", "eagle"]
     bot.bi_lexicon = {"car": "mercedes", "boat": "sail", "train": "track"}
     assert bot.i_filter_random(words) == "car"
 
 
+def test_o_filter_random():
+    u"""Assert the returned element is in the initial list."""
+
+
+        def o_filter_random(self, sentences):
+        u"""Return randomly selected sentence from sentecnces"""
+        return random.choice(sentences)
+
 # untested methods:
-# i_filter_random
 # o_filter_random
 # _create_chains
 # _pair_seed

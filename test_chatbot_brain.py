21b884876ad211851b0c8954a1cc3e4b42cae11e
jefrailey <jcaetof@gmail.com>
2014-06-25 19:44:13 -0700

    Add test_i_filter_random_empty_words() to assert that a list that contains an empty string will return the stock phrase

--- test_chatbot_brain.py
+++ test_chatbot_brain.py
@@ -1,5 +1,4 @@
 import chatbot_brain
-import input_filters
 
 
 def test_initialize_bot():
@@ -21,3 +20,18 @@ def test_compose_response():
     assert "," not in output[0]
     for sentence in output:
         assert "." not in sentence[:-1]
+
+
+def test_i_filter_random_empty_words():
+    u"""Assert the returned word is in the lexicon and is not a stop char."""
+    bot = chatbot_brain.Chatbot()
+    words = [""]
+    assert bot.i_filter_random(words) == u"What a funny thing to say!"
+
+# untested methods:
+# i_filter_random
+# o_filter_random
+# _create_chains
+# _pair_seed
+# _chain_filters
+# _filter_recursive

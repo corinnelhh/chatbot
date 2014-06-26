e90c944acccc4de662a5ff4a72b2e326803beedd
corinnelhh <corinne.hutchinson@gmail.com>
2014-06-25 21:24:30 -0700

    removed debug-True in app

--- views.py
+++ views.py
@@ -45,6 +45,6 @@ if __name__ == '__main__':
     cbot = chatbot_brain.Chatbot()
     cbot.fill_lexicon()
     app.run(debug=True)
-    #from wsgiref.simple_server import make_server
-    #srv = make_server('localhost', 8000, app)
-    #srv.serve_forever()#
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8000, app)
+    srv.serve_forever()
